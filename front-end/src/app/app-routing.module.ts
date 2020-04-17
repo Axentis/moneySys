226d6014efae5ce528e0d97154543ed80d2822d5
ChouhabAmine <aminechouhab@gmail.com>
2020-04-17 11:36:01 +0100

    Version final de formulaire

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PorteurComponent } from './porteur/porteur.component';
 
-
 const routes: Routes = [
   {path:'porteur', component:PorteurComponent}
 ];
